4c0e3e3b6efa93e01bbbaa5fd66925b778e46200
Miyyer Gaitan <miyyercadc@hotmail.com>
2021-06-24 23:59:37 -0500

    Header

---
+++ src/components/WifiPage.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import Selector_rgb from './Selector_rgb';
+
+export function WifiPage() {
+    var page = <>
+    <Selector_rgb />
+    </>
+    return page;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Switch, Route, NavLink} from "react-router-dom";
+import { Feather, ShoppingCart } from 'react-feather';
 
-ReactDOM.render(
-    <App />,
-document.getElementById('app')
-);
+import {GlobalStyle, Menu, Header, Page, Hamburger} from './components/UiComponents'
+
+if (process.env.NODE_ENV === 'production')
+    var url = window.location.origin;
+else
+    var url = 'http://192.168.1.54';
+
+function Root() {
+    
+    const [menu, setMenu] = useState(false);
+
+    return <><GlobalStyle />
+
+    <BrowserRouter>
+
+        <Header>
+            <h1><Feather style={{vericalAlign:"-0.1em"}} /> Restaurant</h1>
+            <h1><ShoppingCart style={{verticalAlign:"-0.1em"}} /> </h1>
+            <Hamburger onClick={() => setMenu(!menu)} />
+            <Menu className={menu ? "" : "menuHidden"}>
+                <li><NavLink onClick={() => setMenu(false)} exact to="/">Home</NavLink></li>
+                <li><NavLink onClick={() => setMenu(false)} exact to="/config">Check-out</NavLink></li>
+            </Menu>
+
+        </Header>
+        
+    </BrowserRouter>
+</>;
+
+}
+
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
